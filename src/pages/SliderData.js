7628d02f0fcb2f74fbd083a144736ba43129a546
Ashif Pathan <ashifbgmiking@gmail.com>
2025-02-28 10:08:58 +0530

    Update SliderData.js

--- src/pages/SliderData.js
+++ src/pages/SliderData.js
@@ -16,6 +16,32 @@ const SliderData = [
     alt: 'Image 2',
     width: 20,
     text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },{
+    id: 3 ,
+    name: 'Balram Meena',
+    job: 'Java Developer',
+    image: 'https://media.licdn.com/dms/image/v2/D4E03AQEbD2_0jRmNAA/profile-displayphoto-shrink_100_100/B4EZVHdUisHcAU-/0/1740660625642?e=2147483647&v=beta&t=cXD01gbX28kyKCFPt9HVEIVB4K50zFDpbjcaIb6N8Ks',
+    alt:'Image 3',
+    width: 20,
+    text: 'I am a dedicated Java Developer , I give my best to this company '
+  }
+  ,{
+    id: 4 ,
+    name: 'Yash Nayak',
+    job: 'App Developer',
+    image: 'https://media.licdn.com/dms/image/v2/D5603AQEZOCansLtrDQ/profile-displayphoto-shrink_100_100/B56ZVG2kvoGQAY-/0/1740650470934?e=2147483647&v=beta&t=dLAYju9pHPjKRNnU8-c2Yfe-qpfq_A8ChX_fSFt3jRA',
+    alt:'Image 4',
+    width: 20,
+    text: 'I am a dedicated App Developer , I give my best to this company '
+  }
+  ,{
+    id: 5 ,
+    name: 'Harsh Dhabi',
+    job: 'Web Developer',
+    image: 'https://media.licdn.com/dms/image/v2/D4E03AQFf62RHOaZr8Q/profile-displayphoto-shrink_400_400/B4EZPG4K84HwAo-/0/1734208436020?e=2147483647&v=beta&t=lx4hih5e2CYEUf_0dZzwhaTVgYuxBX2oXVVWHGyG4nI',
+    alt:'Image 5',
+    width: 20,
+    text: 'I am a dedicated JaWebva Developer , I give my best to this company '
   }
 ];
   
